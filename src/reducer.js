29904c2230c4ab7d1290e528e2bdddf1ad9bb14b
Haoran Zhou <kens0serious@gmail.com>
2022-02-05 01:44:18 -0500

    added increase and decrease item amount function

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,5 @@
+import CartItem from './CartItem'
+
 const reducer = (state, action) => {
   if (action.type === 'CLEAR_CART') {
     return { ...state, cart: [] }
@@ -7,6 +9,29 @@ const reducer = (state, action) => {
     const newCart = state.cart.filter((item) => item.id !== action.payload)
     return { ...state, cart: newCart }
   }
+
+  if (action.type === 'INCREASE') {
+    const newCart = state.cart.map((item) => {
+      if (item.id === action.payload) {
+        return { ...item, amount: item.amount + 1 }
+      }
+      return item
+    })
+    return { ...state, cart: newCart }
+  }
+
+  if (action.type === 'DECREASE') {
+    const newCart = state.cart
+      .map((item) => {
+        if (item.id === action.payload) {
+          return { ...item, amount: item.amount - 1 }
+        }
+        return item
+      })
+      .filter((item) => item.amount !== 0)
+    return { ...state, cart: newCart }
+  }
+
   return state
 }
 export default reducer
